bad38874f62ec4c6f2667830b1b30491198fe179
malkyfried <hand1b22@bjcomp.org>
2023-12-28 23:18:25 +0200

    Create button to add new post and display it in users posts list

--- src/userPosts/Posts.js
+++ src/userPosts/Posts.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { CircularProgress, List, ListItem, ListItemText, Typography } from "@mui/material";
-import './Posts.css';
+import { Button, CircularProgress, List, ListItem, ListItemText, Typography } from "@mui/material";
 import CustomScrollbar from "../shared/scrollbar";
+import CreatePost from "../newPost/createPost";
+import './Posts.css';
 
 const UserPosts = ({ userId }) => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -27,24 +29,51 @@ const UserPosts = ({ userId }) => {
     fetchPosts();
   }, [userId]);
 
+  const handleDialogOpen = () => {
+    setIsDialogOpen(true);
+  };
+
+  const handleDialogClose = () => {
+    setIsDialogOpen(false);
+  };
+
+  const handlePostCreated = (newPost) => {
+    //show posts where all posts are displayed
+    console.log("new post has been created")
+    setPosts((prevPosts) => [newPost, ...prevPosts]);
+  };
+
   return (
     <div className="user-posts">
-    <CustomScrollbar>
-      <Typography variant="h1">Posts by User</Typography>
-      {loading ? (
-        <CircularProgress className="loading-spinner"/>
-      ) : error ? (
-        <p className="error-message">{error}</p>
-      ) : (
-        <List>
-          {posts.map(post => (
-            <ListItem key={post.id}>
-              <ListItemText primary={post.title} />
-            </ListItem>
-          ))}
-        </List>
-      )}
-    </CustomScrollbar>
+      <CustomScrollbar>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Typography variant="h1">Posts by User</Typography>
+          <div style={{ marginRight: 'auto' }}>
+            <Button variant="contained" onClick={handleDialogOpen} style={{ backgroundColor: '#4caf50', color: '#fff', marginLeft: '22px' }}>
+              Create Post
+            </Button>
+          </div>
+        </div>
+        <CreatePost
+          open={isDialogOpen}
+          onClose={handleDialogClose}
+          userId={userId}
+          onPostCreated={handlePostCreated}
+        />
+        {loading ? (
+          <CircularProgress className="loading-spinner" />
+        ) : error ? (
+          <p className="error-message">{error}</p>
+        ) : (
+          <List>
+            {posts.map(post => (
+              <ListItem key={post.id}>
+                <ListItemText primary={post.title} />
+              </ListItem>
+            ))}
+          </List>
+        )}
+      </CustomScrollbar>
     </div>
   );
 };
